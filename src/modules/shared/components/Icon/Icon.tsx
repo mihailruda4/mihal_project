e52bf8f8439058fcf4ac6775328e0489ace7c05b
mihailruda4 <mihailruda4@gmail.com>
2025-09-25 15:12:42 +0300

    vlad's_project

---
+++ src/modules/shared/components/Icon/Icon.tsx
@@ -0,0 +1,12 @@
+import { FC, memo } from 'react';
+
+type Props = {
+  icon: { title: string; path: string };
+  className?: string;
+};
+
+export const Icon: FC<Props> = memo(({ icon, className }) => (
+  <img src={icon.path} alt={icon.title} className={className} />
+));
+
+Icon.displayName = 'icon';
---
+++ src/modules/shared/components/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+import Styles from './Loader.module.scss';
+
+export const Loader = () => (
+  <div className={Styles.Loader}>
+    <div className={Styles.Loader__content} />
+  </div>
+);
